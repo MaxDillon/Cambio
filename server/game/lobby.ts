9d2610f37a37be73b4a2bcca508b4230c516b89f
MaxDillon <maxdillon13124@gmail.com>
2022-05-06 13:36:36 -0400

    added json web token info and authentication

--- server/game/lobby.ts
+++ server/game/lobby.ts
@@ -10,17 +10,36 @@ RecreatingLobyException.prototype = Object.create(Error.prototype);
 RecreatingLobyException.prototype.name = "RecreatingLobyException";
 RecreatingLobyException.prototype.constructor = RecreatingLobyException;
 
+
+class Player {
+	id: number;
+	nick: string;
+	isReady: boolean;
+
+	constructor(lobby: Lobby, nick: string) {
+		this.isReady = false;
+		this.nick = nick;
+		this.id = lobby.players.length;
+		lobby.players[this.id] = this;
+	}
+}
+
 class Lobby {
 	static lobbies = {};
-	players: number;
+	players: Player[];
 	constructor(lobby_id: string) {
 		if (lobby_id in Lobby.lobbies) {
 			throw RecreatingLobyException("");
 		}
 		Lobby.lobbies[lobby_id] = this;
-		this.players = 0;
+		this.players = [];
 	}
 
+
+	addUser(nick: string): number {
+		const player = new Player(this, nick);
+		return player.id
+	}
 
 }
 
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,19 +1,28 @@
 import express from 'express'
 import { Request, Response } from 'express'
 import Lobby from '../game/lobby'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
-
+const SECRET = 'asdfaksdfkladfasdfkasdkfjlsd'
 router.post('/enterLobby', (req: Request, res: Response) => {
 	const id_hash = req.body.id_hash;
+	const nick = req.body.nick;
+
 	var lobby: Lobby;
 	if (id_hash in Lobby.lobbies) {
 		lobby = Lobby.lobbies[id_hash];
 	} else {
-		lobby = new Lobby(id_hash);
+		lobby = new Lobby(id_hash);		 
 	}
-	res.cookie('roomID', id_hash, {httponly: true})
-	res.send({})
+	const player_number = lobby.addUser(nick)
+	const webToken = jwt.sign({id_hash: id_hash, player_number: player_number}, SECRET)
+
+	res.cookie('roomJsonToken', webToken, {httpOnly: true, maxAge: 360000, secure: true, sameSite: true})
+	res.send({
+		err: false,
+		message: 'added to lobby'
+	})
 })
 
 
